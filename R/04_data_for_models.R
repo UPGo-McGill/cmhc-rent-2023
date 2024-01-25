860e61875095e953607d0f0293163d0fb496b071
dwachsmuth <david.wachsmuth@mcgill.ca>
2024-01-25 08:54:45 -0500

    Model data update to remove vacancy, since it has a very high level of NA observations

--- R/04_data_for_models.R
+++ R/04_data_for_models.R
@@ -10,16 +10,15 @@ data_rent <-
   st_as_sf() |> 
   filter(FREH_share < 1, rev_share < 1) |>
   mutate(year = as.character(year)) |> 
-  mutate(across(c(rent, FREH_share, rev_share, universe, vacancy, pop_CMA, 
+  mutate(across(c(rent, FREH_share, rev_share, universe, pop_CMA, 
                   accommodation), .fns = list(log = log), 
                 .names = "{.fn}_{.col}")) |> 
   select(id, year, CMA, province, rent, log_rent, FREH_share, log_FREH_share, 
-         rev_share, log_rev_share, universe, log_universe, vacancy, log_vacancy,
-         tenant_share, pop_CMA, log_pop_CMA, accommodation, 
-         log_accommodation) |> 
+         rev_share, log_rev_share, universe, log_universe, tenant_share, 
+         pop_CMA, log_pop_CMA, accommodation, log_accommodation) |> 
   filter(!is.infinite(log_FREH_share), !is.infinite(log_rev_share),
-         !is.infinite(log_universe), !is.infinite(log_vacancy),
-         !is.infinite(log_pop_CMA), !is.infinite(log_accommodation)) |> 
+         !is.infinite(log_universe), !is.infinite(log_pop_CMA), 
+         !is.infinite(log_accommodation)) |> 
   na.omit()
 
 data_change <- 
@@ -30,12 +29,11 @@ data_change <-
          !is.infinite(rev_change_pct)) |>
   filter(abs(FREH_change_pct) < 5, abs(rev_change_pct) < 5) |>
   mutate(year = as.character(year)) |> 
-  mutate(across(c(universe, vacancy, pop_CMA, accommodation), 
+  mutate(across(c(universe, pop_CMA, accommodation), 
                 .fns = list(log = log), .names = "{.fn}_{.col}")) |> 
   select(id, year, CMA, province, rent_change_pct, FREH_change_pct, FREH_share,
-         rev_change_pct, rev_share, universe, log_universe, vacancy, 
-         log_vacancy, tenant_share, pop_CMA, log_pop_CMA, accommodation, 
-         log_accommodation) |> 
-  filter(!is.infinite(log_universe), !is.infinite(log_vacancy),
-         !is.infinite(log_pop_CMA), !is.infinite(log_accommodation)) |> 
+         rev_change_pct, rev_share, universe, log_universe, tenant_share, 
+         pop_CMA, log_pop_CMA, accommodation, log_accommodation) |> 
+  filter(!is.infinite(log_universe), !is.infinite(log_pop_CMA), 
+         !is.infinite(log_accommodation)) |> 
   na.omit()
