0519ded8c79689f1b0e46f51673aacf41933aadb
dwachsmuth <david.wachsmuth@mcgill.ca>
2024-05-20 13:44:48 -0400

    DAG processing

---
+++ R/06_process_DAGs.R
@@ -0,0 +1,15 @@
+#### Process DAGs ##############################################################
+
+library(magick)
+
+magick::image_read("data/dagitty_FREH.png") |> 
+  image_crop("1800x1370+294+100") |> 
+  image_write("output/figure_4a.png")
+
+magick::image_read("data/dagitty_rev.png") |> 
+  image_crop("1800x1370+294+100") |> 
+  image_write("output/figure_4b.png")
+
+magick::image_read("data/dagitty_price.png") |> 
+  image_crop("1800x1370+294+100") |> 
+  image_write("output/figure_4c.png")
