c1a8f56678e02c96a91531b1b8841d6f7b24d648
dwachsmuth <david.wachsmuth@mcgill.ca>
2024-05-28 23:09:43 -0400

    DAG updates

--- R/06_process_DAGs.R
+++ R/06_process_DAGs.R
@@ -2,26 +2,26 @@
 
 library(magick)
 
-magick::image_read("data/dagitty_FREH.png") |> 
-  image_crop("1600x1300+790+130") |> 
+image_read("data/dagitty_FREH.png") |> 
+  image_crop("2250x1040+50+380") |> 
   image_write("output/figure_4a.png")
 
-magick::image_read("data/dagitty_rev.png") |> 
-  image_crop("1600x1300+790+130") |> 
+image_read("data/dagitty_rev.png") |> 
+  image_crop("2250x1040+50+380") |> 
   image_write("output/figure_4b.png")
 
-magick::image_read("data/dagitty_price.png") |> 
-  image_crop("1600x1300+790+130") |> 
+image_read("data/dagitty_price.png") |> 
+  image_crop("2250x1040+50+380") |> 
   image_write("output/figure_4c.png")
 
-magick::image_read("data/dagitty_FREH_change.png") |> 
-  image_crop("1600x1300+790+130") |> 
+image_read("data/dagitty_FREH_change.png") |> 
+  image_crop("2250x1040+50+380") |> 
   image_write("output/figure_4d.png")
 
-magick::image_read("data/dagitty_rev_change.png") |> 
-  image_crop("1600x1300+790+130") |> 
+image_read("data/dagitty_rev_change.png") |> 
+  image_crop("2250x1040+50+380") |> 
   image_write("output/figure_4e.png")
 
-magick::image_read("data/dagitty_price_change.png") |> 
-  image_crop("1600x1300+790+130") |> 
+image_read("data/dagitty_price_change.png") |> 
+  image_crop("2250x1040+50+380") |> 
   image_write("output/figure_4f.png")
