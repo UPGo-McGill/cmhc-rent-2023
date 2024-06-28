e0991398602aebb0e5b8836ef4b6c7bd28aca46d
dwachsmuth <david.wachsmuth@mcgill.ca>
2024-06-28 17:09:54 -0400

    Script cleanup

--- R/07_descriptives.R
+++ R/07_descriptives.R
@@ -1,33 +1,7 @@
-#### 09 DESCRIPTIVE STATISTICS #################################################
+#### 07 DESCRIPTIVE STATISTICS #################################################
 
-source("R/08_data_for_models.R")
+source("R/06_data_for_models.R")
 qload("output/cmhc.qsm", nthreads = availableCores())
-
-
-# Figure 5: Correlation between STR change and rent_change ----------------
-
-fig_5 <-
-  monthly_sept |> 
-  filter(year >= 2018) |> 
-  st_drop_geometry() |> 
-  select(rent_change, FREH_change, non_FREH_change, price_change) |> 
-  filter(abs(rent_change) < 400,
-         abs(FREH_change) < 0.01,
-         abs(non_FREH_change) < 0.01,
-         abs(price_change) < 400) |> 
-  GGally::ggpairs(aes(size = "fixed", alpha = "fixed"),
-          upper = list(continuous = GGally::wrap(
-            GGally::ggally_cor, display_grid = FALSE, family = "Futura")),
-          lower = list(continuous = GGally::wrap(
-            GGally::ggally_smooth_lm, se = FALSE))) +
-  scale_size_manual(values = c(fixed = 0.2)) +
-  scale_alpha_manual(values = c(fixed = 0.4)) +
-  theme_minimal() +
-  theme(text = element_text(family = "Futura"),
-        axis.text = element_text(size = 5),
-        strip.text = element_text(size = 7))
-
-ggsave("output/figure_5.png", fig_5, width = 8, height = 4, units = "in")
 
 
 # DiD treatment -----------------------------------------------------------
@@ -72,14 +46,14 @@ monthly_sept |>
   mutate(across(c(FREH_change, non_FREH_change, price_change),
                 \(x) if_else(year <= 2018, NA, x))) |> 
   select(rent, rent_change, FREH_change, non_FREH_change, price_change, 
-         vacancy_lag, income, apart, tourism) |> 
+         rent_lag, vacancy_lag, income, apart, tourism) |> 
   st_drop_geometry() |> 
   mutate(vacancy_lag = if_else(
     vacancy_lag == 0, min(vacancy_lag[vacancy_lag > 0]), vacancy_lag)) |> 
   # Create logged versions of all variables
   mutate(across(c(rent:tourism), .fns = list(log = \(x) log(x)))) |> 
   select(rent_log, rent_change, FREH_change, non_FREH_change, price_change, 
-         vacancy_lag_log, income_log, apart_log, tourism_log) |> 
+         rent_lag_log, vacancy_lag_log, income_log, apart_log, tourism_log) |> 
   pivot_longer(everything()) |> 
   summarize(
     n = sum(!is.na(value)),
@@ -90,3 +64,29 @@ monthly_sept |>
     .by = name) |> 
   mutate(across(-c(name, n), \(x) scales::comma(x, 0.001))) |> 
   suppressWarnings()
+
+
+# Figure 4: Correlation between STR change and rent_change ----------------
+
+fig_4 <-
+  monthly_sept |> 
+  filter(year >= 2018) |> 
+  st_drop_geometry() |> 
+  select(rent_change, FREH_change, non_FREH_change, price_change) |> 
+  filter(abs(rent_change) < 400,
+         abs(FREH_change) < 0.01,
+         abs(non_FREH_change) < 0.01,
+         abs(price_change) < 400) |> 
+  GGally::ggpairs(aes(size = "fixed", alpha = "fixed"),
+                  upper = list(continuous = GGally::wrap(
+                    GGally::ggally_cor, display_grid = FALSE, family = "Futura")),
+                  lower = list(continuous = GGally::wrap(
+                    GGally::ggally_smooth_lm, se = FALSE))) +
+  scale_size_manual(values = c(fixed = 0.2)) +
+  scale_alpha_manual(values = c(fixed = 0.4)) +
+  theme_minimal() +
+  theme(text = element_text(family = "Futura"),
+        axis.text = element_text(size = 5),
+        strip.text = element_text(size = 7))
+
+ggsave("output/figure_4.png", fig_4, width = 8, height = 4, units = "in")
